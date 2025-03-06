ab6ac4e983f28c1f80145e8594658550f4cc6178
Ajay Shrivastava <97840882+Ajay-Shrivastava@users.noreply.github.com>
2025-03-06 16:14:24 +0530

    Create variables.tf

---
+++ ACR_RoleAssignment/variables.tf
@@ -0,0 +1,9 @@
+variable "acr_id" {
+    description = "The ID of the Azure Container Registry"
+    type        = string
+}
+
+variable "principal_id" {
+    description = "The ID of the principal"
+    type        = string
+}
